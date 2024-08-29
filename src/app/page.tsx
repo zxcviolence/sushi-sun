4419a00e3125124ee43da748c54060d51ea3e6cf
Dexter <yakubov959@mail.ru>
2024-08-29 15:38:51 +0300

    /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home <Button variant="outline">Корзина</Button></h1>
+      
+    </>
   );
 }
